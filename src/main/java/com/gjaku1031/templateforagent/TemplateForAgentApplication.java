da8ee72080145513b268ecdbd10a7dd7632adf36
gjaku1031 <gjaku1031@gmail.com>
2025-10-30 12:27:36 +0900

    Init: First Commit

---
+++ src/main/java/com/gjaku1031/templateforagent/TemplateForAgentApplication.java
@@ -0,0 +1,13 @@
+package com.gjaku1031.templateforagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateForAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateForAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gjaku1031/templateforagent/TemplateForAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjaku1031.templateforagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateForAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
