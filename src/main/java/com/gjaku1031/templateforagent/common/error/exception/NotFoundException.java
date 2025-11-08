5a350aa25fbb82e6143143b538c9f6c36c3cb0f4
gjaku1031 <gjaku1031@users.noreply.github.com>
2025-11-08 18:39:41 +0900

    feat(common): add NotFoundException

---
+++ src/main/java/com/gjaku1031/templateforagent/common/error/exception/NotFoundException.java
@@ -0,0 +1,11 @@
+package com.gjaku1031.templateforagent.common.error.exception;
+
+/**
+ * 리소스가 존재하지 않을 때 발생하는 예외
+ */
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
+
